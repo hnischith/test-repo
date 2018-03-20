7e4f070ae6472d970221f375250e5632a82160a5
Nischith Honnavar <nischith.96@gmail.com>
2018-03-20 22:10:39 +0530

    Add functionality added!

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
+#include "functionalities.h"
 int main()
 {
 	int a, b;
 	cout << "Enter the values for a and b"<<endl;
 	cin<<a<<b;
 	cout <<"a: "<<a<<", b:"<<b<<endl
+	cout<<"a + b = "<<add(a,b)<<endl;
 	return 0;
 }
