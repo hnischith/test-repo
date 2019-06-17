cbb5fd494c1006571d85198698b9f3200dfe8603
Nischith Honnavar <nischith.96@gmail.com>
2019-06-17 21:25:17 +0530

    Onda, yerada. Yest changes guru

--- some_dependent_headder_file.h
+++ some_dependent_headder_file.h
@@ -1,3 +1,12 @@
 #include "mycleanheadder.h"
 #include "somebodyelsesheadder.h"
 #include "ayyoiforgottoaddthisheadder.h"
+enum states_of_blah{
+RUN,
+PLAY,
+PAUSE,
+REWIND,
+FORWARD,
+KILL,
+NUMBER_OF_STATES
+};
