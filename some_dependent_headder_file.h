140f81c02db57dc558e438f460ad82dd693705e9
Nischith Honnavar <nischith.96@gmail.com>
2019-06-17 22:19:35 +0530

    Added the publisher details

--- some_dependent_headder_file.h
+++ some_dependent_headder_file.h
@@ -1,7 +1,10 @@
+//Author Name: Nischith Honnnavar
+//Email: nischith.96@gmail.com
 #include "mycleanheadder.h"
 #include "somebodyelsesheadder.h"
 #include "ayyoiforgottoaddthisheadder.h"
-enum states_of_blah{
+enum states_of_blah
+{
 RUN,
 PLAY,
 PAUSE,
